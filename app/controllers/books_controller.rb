086fe688bed4bbc0e9a55a565957c49dc08cade0
Alexander <s7efanov@gmail.com>
2018-01-24 15:40:48 +0000

    CRUD Request

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,4 +13,40 @@ class BooksController < ApplicationController
         @book = Book.new
     end
     
+    def create
+        params.require(:book)
+        permitted = params[:book].permit(:title, :genre, :isbn_number, :publish_date)
+        @book = Book.new(permitted)
+        if @book.save
+            flash[:notice] = "#{@book.title} was successfully created."
+            redirect_to books_path
+        else
+            render 'new' # note, 'new' template can access @book's field values!
+        end
+    end
+    
+    def edit
+        @book = Book.find params[:id]
+    end
+    
+    def update
+        @book = Book.find params[:id]
+        params.require(:book)
+        permitted = params[:book].permit(:title, :genre, :isbn_number, :publish_date)
+        
+        if @book.update_attributes!(permitted)
+            flash[:notice] = "#{@book.title} was successfully updated."
+            redirect_to books_path
+        else
+            render 'edit' # note, 'edit' template can access @book;s field values!
+        end
+    end
+    
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        flash[:notice] = "Book '#{@book.title}' deleted."
+        redirect_to books_path
+    end
+    
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,20 @@
 class Book < ActiveRecord::Base
+    def self.all_genres ; %W[Science-fiction Drama Action-and-Adventure Romance Mystery Horror] ; end
+    validates :title, :presence => true
+    validates :publish_date, :presence => true
+    validate :published_1930_or_later
+    validates :genre, :inclusion => {:in => Book.all_genres},
+        :unless => :grandfathered?
+        
+    def published_1930_or_later
+        errors.add(:publish_date, 'must be 1930 ot later') if
+            publish_date && publish_date < Date.parse('1 Jan 1930')
+    end
+    
+    @@grandfathered_date = Date.parse('1 Nov 1968')
+    
+    def grandfathered?
+        publish_date && publish_date < @@grandfathered_date
+    end
+    
 end
