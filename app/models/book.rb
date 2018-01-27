131541d832215a6124ed9ee92db9bbf90e1b9bc0
Alexander <s7efanov@gmail.com>
2018-01-27 12:13:27 +0000

    final project

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book < ActiveRecord::Base
-    def self.all_genres ; %w[Science-fiction Drama Action-and-Adventure Romance Mystery Horror] ; end
+    def self.all_genres ; %w[Science-Fiction Drama Action-And-Adventure Romance Mystery Horror] ; end
     validates :title, :presence => true
     validates :publish_date, :presence => true
     validate :published_1930_or_later
+    validates :genre, :inclusion => {:in => Book.all_genres},
+        :unless => :grandfathered?
     
     def published_1930_or_later
         errors.add(:publish_date, 'must be 1930 ot later') if
